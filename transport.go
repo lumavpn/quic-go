a43b314b7110e0ffd80b6afa2f5346eebb525e11
lumavpn <admin@lumavpn.com>
2024-07-02 22:51:36 -0700

    Add SetCreatedConn

--- transport.go
+++ transport.go
@@ -552,3 +552,11 @@ func setTLSConfigServerName(tlsConf *tls.Config, addr net.Addr, host string) {
 	}
 	tlsConf.ServerName = h
 }
+
+func (t *Transport) SetCreatedConn(createdConn bool) {
+	t.createdConn = createdConn
+}
+
+func (t *Transport) SetSingleUse(isSingleUse bool) {
+	t.isSingleUse = isSingleUse
+}
